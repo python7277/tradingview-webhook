62aab2a6e6e02c3c7f10106c5159060994ca5f76
python7277 <python7277@gmail.com>
2026-03-11 02:03:00 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "TradingView Webhook Running"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print(data)
+    return {"status": "received"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
